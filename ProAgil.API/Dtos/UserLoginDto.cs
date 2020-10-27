3863b48446ea3a114a378afcfd2321e27703def4
Fabio <fabio.christovam@fatec.sp.gov.br>
2020-10-27 11:34:20 -0300

    Inserindo Identity Core .NET

---
+++ ProAgil.API/Dtos/UserLoginDto.cs
@@ -0,0 +1,10 @@
+namespace ProAgil.API.Dtos
+{
+    public class UserLoginDto
+    {
+        public string UserName { get; set; }
+       
+        public string Password { get; set; }
+       
+    }
+}
